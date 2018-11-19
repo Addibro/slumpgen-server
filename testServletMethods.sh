b79448ee62d0efaff2f8c5d86aadf1db7db7259f
Andreas Alme <andreas.alme.eriksson@gmail.com>
2018-11-19 19:30:11 +0100

    script fix

--- testServletMethods.sh
+++ testServletMethods.sh
@@ -2,7 +2,7 @@
 
 DATA='{"name": "andy", "nickname": "andypandy"}, {"name": "david", "nickname": "davipady"}'
 
-echo -e "Testing doGet...\n\n"
+echo -e "\n\n--- Testing doGet... ---\n\n"
 curl -i http://localhost:8080/characters/get
-echo -e "\n\nTesting doPost using ${DATA}\n"
+echo -e "\n\n--- Testing doPost using ${DATA} ---\n\n"
 curl -i -d "$DATA" -X POST http://localhost:8080/characters/add
