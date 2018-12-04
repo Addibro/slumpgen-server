a0a68fd35087ed489a4d33b10e2eba4a6136578d
Andreas Alme <andreas.alme.eriksson@gmail.com>
2018-12-04 11:50:11 +0100

    better curl test

--- testServletMethods.sh
+++ testServletMethods.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-DATA='{"name": "andy", "kin": "human"}, {"name": "david", "kin": "human"}'
-RES='creeds'
-TYPE='json'
+if [[ $# -ne 2 ]]
+then
+    echo "usage: testServletMethods [json] [names/nicknames/creeds]"
+    exit 1
+fi
+TYPE=$1
+RES=$2
 echo -e "\n\n--- Testing doGet using localhost:8080/characters/fetch?type=$TYPE&res=$RES ---\n\n"
 sleep 1
-
-curl -s http://localhost:8080/characters/fetch?type=$TYPE&res=$RES
-sleep 1
-echo -e "\n\n--- Testing doPost using ${DATA} ---\n\n"
-sleep 1
-curl -i -d "$DATA" -X POST http://localhost:8080/characters/add
+# need to quote it since shell sees & as the end of the command
+curl -s "http://localhost:8080/characters/fetch?type=$TYPE&res=$RES" | jq
