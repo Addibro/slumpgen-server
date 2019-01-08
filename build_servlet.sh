8e691a5dde122210441322fe15d861783f731799
Andreas Alme <andreas.alme.eriksson@gmail.com>
2019-01-08 15:40:23 +0100

    remove json/

--- build_servlet.sh
+++ build_servlet.sh
@@ -11,8 +11,3 @@ fi
 
 echo "Compiling..."
 javac -Xlint:unchecked -cp winstone.jar${SEPARATOR}www/WEB-INF/classes -d bin src/com/alming/slumpgen/storage/*.java src/com/web/http/*.java src/com/web/servlets/*.java
-#javac -Xlint:unchecked -cp winstone.jar${SEPARATOR}www/WEB-INF/classes -d www/WEB-INF/classes src/com/alming/slumpgen/storage/*.java src/com/web/http/*.java src/com/web/servlets/*.java
-
-
-
-# [[ $? = 0 ]] && echo -e "\nCompilation successful!" || echo -e "\n\nError compiling Servlet.java"
