1910e82427c24444a3055be5e83e3ad1d02e4608
Andreas Alme <andreas.alme.eriksson@gmail.com>
2019-04-02 14:36:43 +0200

    refactor QueryParser

--- src/com/web/http/QueryParser.java
+++ src/com/web/http/QueryParser.java
@@ -7,20 +7,30 @@ import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.Optional;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 /**
- *  //TODO
+ * //TODO
  */
 public class QueryParser {
 
-    private QueryParser() {}
+    private static Function<String, String> key = keyString -> keyString.split("=")[0];
+    private static Function<String, String> value = valueString -> valueString.split("=")[1];
+
+    private QueryParser() {
+    }
 
     public static Map<String, String> parse(String querystring) throws IllegalArgumentException {
-        if (!querystring.matches("^type=[^=]+&res=[^=]+")) throw new IllegalArgumentException("Illegal argument(s) in query");
+        if (invalidQuery(querystring))
+            throw new IllegalArgumentException("Illegal argument(s) in query");
         Map<String, String> queries = Arrays.stream(querystring.split("&"))
-            .collect(Collectors.toMap(k -> k.split("=")[0], v -> v.split("=")[1]));
+                .collect(Collectors.toMap(QueryParser.key, QueryParser.value));
         return queries;
+    }
+
+    private static boolean invalidQuery(String querystring) {
+        return !querystring.matches("^type=[^=]+&res=[^=]+");
     }
 }
