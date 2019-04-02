976f4a31a6444da4dd5fbca338e4a08da1258e88
Andreas Alme <andreas.alme.eriksson@gmail.com>
2019-04-02 14:37:18 +0200

    remove TODO

--- src/com/web/http/QueryParser.java
+++ src/com/web/http/QueryParser.java
@@ -11,9 +11,6 @@ import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
-/**
- * //TODO
- */
 public class QueryParser {
 
     private static Function<String, String> key = keyString -> keyString.split("=")[0];
